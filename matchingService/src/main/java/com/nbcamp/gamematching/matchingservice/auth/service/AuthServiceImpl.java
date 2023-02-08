79e8f88c7e3cfcfcc398b432019d07490f5677be
arkt0956 <55053131+arkt0956@users.noreply.github.com>
2023-02-08 16:22:44 +0900

    feat: commit error 수정

--- matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/auth/service/AuthServiceImpl.java
+++ matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/auth/service/AuthServiceImpl.java
@@ -2,12 +2,25 @@ package com.nbcamp.gamematching.matchingservice.auth.service;
 
 import com.nbcamp.gamematching.matchingservice.auth.dto.SigninRequest;
 import com.nbcamp.gamematching.matchingservice.auth.dto.SignupRequest;
+<<<<<<< Updated upstream
 import com.nbcamp.gamematching.matchingservice.jwt.JwtUtil;
 import com.nbcamp.gamematching.matchingservice.member.domain.MemberRoleEnum;
 import com.nbcamp.gamematching.matchingservice.member.entity.Member;
 import com.nbcamp.gamematching.matchingservice.member.repository.MemberRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+=======
+import com.nbcamp.gamematching.matchingservice.exception.ExistsException;
+import com.nbcamp.gamematching.matchingservice.exception.NotfoundException;
+import com.nbcamp.gamematching.matchingservice.exception.SignException;
+import com.nbcamp.gamematching.matchingservice.jwt.JwtUtil;
+import com.nbcamp.gamematching.matchingservice.member.entity.Member;
+import com.nbcamp.gamematching.matchingservice.member.entity.MemberRoleEnum;
+import com.nbcamp.gamematching.matchingservice.member.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Not;
+>>>>>>> Stashed changes
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,9 +38,16 @@ public class AuthServiceImpl implements AuthService {
     public void signUp(SignupRequest signupRequest) {
         String email = signupRequest.getEmail();
         String password = signupRequest.getPassword();
+<<<<<<< Updated upstream
         if (memberRepository.existsByEmail(email)) {
             throw new IllegalArgumentException("이메일 없음");
         } //커스텀 익셉션
+=======
+
+        if(memberRepository.existsByEmail(email)) {
+            throw new ExistsException.DuplicatedEmail();}
+
+>>>>>>> Stashed changes
         String encodedPassword = passwordEncoder.encode(password);
         Member member = Member.builder()
                 .email(email)
@@ -42,14 +62,24 @@ public class AuthServiceImpl implements AuthService {
     public void signIn(SigninRequest signinRequest, HttpServletResponse response) {
         String email = signinRequest.getEmail();
         String password = signinRequest.getPassword();
+<<<<<<< Updated upstream
         Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
         if (!passwordEncoder.matches(password, member.getPassword())) {
             throw new IllegalArgumentException("비밀번호 불일치");
+=======
+
+        Member member = memberRepository.findByEmail(email).orElseThrow(SignException::new);
+        if(!passwordEncoder.matches(password, member.getPassword())) {
+            throw new SignException();
+>>>>>>> Stashed changes
         }
         response.addHeader(JwtUtil.AUTHORIZATION_HEADER,
                 jwtUtil.createToken(member.getEmail(), member.getRole()));
     }
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 }
--- matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/security/UserDetailsServiceImpl.java
+++ matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/security/UserDetailsServiceImpl.java
@@ -1,11 +1,18 @@
 package com.nbcamp.gamematching.matchingservice.security;
 
+<<<<<<< Updated upstream
+=======
+import com.nbcamp.gamematching.matchingservice.exception.NotFoundException;
+>>>>>>> Stashed changes
 import com.nbcamp.gamematching.matchingservice.member.entity.Member;
 import com.nbcamp.gamematching.matchingservice.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+<<<<<<< Updated upstream
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+=======
+>>>>>>> Stashed changes
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +22,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) {
         Member member = memberRepository.findByEmail(email)
+<<<<<<< Updated upstream
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+=======
+                .orElseThrow(() -> new NotFoundException.NotFoundMemberException());
+>>>>>>> Stashed changes
         return new UserDetailsImpl(member, member.getEmail(), member.getPassword());
     }
 }
