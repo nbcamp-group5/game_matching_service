747fcc98ca6527e5406cabf98ca36947f4fe669f
Jeon Jaehyeong <wjswogud789@gmail.com>
2023-02-28 10:30:14 +0900

    feat: Member에 provider 추가

--- matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/member/entity/Member.java
+++ matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/member/entity/Member.java
@@ -1,25 +1,18 @@
 package com.nbcamp.gamematching.matchingservice.member.entity;
 
-import static java.util.regex.Pattern.matches;
-
 import com.nbcamp.gamematching.matchingservice.chat.entity.ChatRoom;
 import com.nbcamp.gamematching.matchingservice.exception.SignException;
 import com.nbcamp.gamematching.matchingservice.member.domain.MemberRoleEnum;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.regex.Pattern.matches;
 
 @Entity
 @Getter
@@ -48,11 +41,15 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private MemberRoleEnum role;
 
+    private String provider;
+
+    private String providerId;
+
     /**
      * 생성자 - 약속된 형태로만 생성가능하도록 합니다.
      */
     @Builder
-    public Member(String email, String password, Profile profile, MemberRoleEnum role) {
+    public Member(String email, String password, Profile profile, MemberRoleEnum role, String provider, String providerId) {
         if (matches("\\w+@\\w+\\.\\w+(\\.\\w+)?", email)) {
             this.email = email;
         } else {
@@ -63,6 +60,8 @@ public class Member {
         if (MemberRoleEnum.isContains(role)) {
             this.role = role;
         }
+        this.provider = provider;
+        this.providerId = providerId;
     }
 
     /**
--- matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/member/repository/MemberRepository.java
+++ matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/member/repository/MemberRepository.java
@@ -1,14 +1,9 @@
 package com.nbcamp.gamematching.matchingservice.member.repository;
 
-import com.nbcamp.gamematching.matchingservice.member.domain.BuddiesMapping;
 import com.nbcamp.gamematching.matchingservice.member.entity.Member;
-import java.util.Optional;
-
-import jakarta.persistence.EntityManager;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -17,4 +12,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
 
     Optional<Member> findByProfileNickname(String friendNick);
+
+    Member findByEmailAndProvider(String email, String provider);
 }
