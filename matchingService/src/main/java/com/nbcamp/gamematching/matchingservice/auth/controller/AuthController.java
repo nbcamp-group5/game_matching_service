7856cbde6f29be34a1f041443e70a6231e989fc9
Jeon Jaehyeong <wjswogud789@gmail.com>
2023-03-05 00:07:14 +0900

    feat: OAuth2 프론트 추가

--- matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/auth/controller/AuthController.java
+++ matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/auth/controller/AuthController.java
@@ -1,8 +1,10 @@
 package com.nbcamp.gamematching.matchingservice.auth.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.nbcamp.gamematching.matchingservice.auth.dto.SigninRequest;
 import com.nbcamp.gamematching.matchingservice.auth.dto.SignupRequest;
 import com.nbcamp.gamematching.matchingservice.auth.service.AuthService;
+import com.nbcamp.gamematching.matchingservice.auth.service.KakaoService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+
+    private final KakaoService kakaoService;
 
     @PostMapping("/signup")
     public void signUp(@RequestBody SignupRequest signupRequest) {
@@ -31,6 +35,16 @@ public class AuthController {
     @PostMapping("/signout")
     public void signOut(HttpServletRequest request) {
         authService.signOut(request);
+    }
+
+    @GetMapping("/kakao/callback")
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws UnsupportedEncodingException, JsonProcessingException {
+
+        String token = kakaoService.kakaoLogin(code, response);
+
+        response.setHeader("Authorization", token);
+
+        return "redirect:http://localhost:5555/kakao.html?token="+ token;
     }
 
 
