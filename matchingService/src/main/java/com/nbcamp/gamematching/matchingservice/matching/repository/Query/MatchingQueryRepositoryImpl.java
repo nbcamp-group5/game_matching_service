ef1fb224abca9f3b86a02651e346209014094ddd
antidote <>
2023-03-06 14:17:51 +0900

    feat : 조회쿼리 추가

--- matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/matching/repository/Query/MatchingQueryRepositoryImpl.java
+++ matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/matching/repository/Query/MatchingQueryRepositoryImpl.java
@@ -8,9 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static com.nbcamp.gamematching.matchingservice.matching.entity.QMatchingLog.matchingLog;
 import static com.nbcamp.gamematching.matchingservice.matching.entity.QResultMatching.resultMatching;
