c0b0e2a9181f755975b5582e2fa65cb18d375bc7
arkt0956 <55053131+arkt0956@users.noreply.github.com>
2023-03-07 16:19:09 +0900

    feat: 페이징 처리 id 순으로 정렬:

--- matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/common/domain/CreatePageable.java
+++ matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/common/domain/CreatePageable.java
@@ -2,14 +2,15 @@ package com.nbcamp.gamematching.matchingservice.common.domain;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 
 public class CreatePageable {
 
     public static Pageable createPageable(int page) {
-//        Sort.Direction direction = Sort.Direction.ASC;
-//        Sort sort = Sort.by(direction, "id");
-        Pageable pageable = PageRequest.of(page - 1, 50);
+        Sort.Direction direction = Sort.Direction.ASC;
+        Sort sort = Sort.by(direction, "id");
+        Pageable pageable = PageRequest.of(page - 1, 50, sort);
         return pageable;
     }
 }
