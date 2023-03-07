05f2b07b5514b2fc44d0a0386975589238591cc9
arkt0956 <55053131+arkt0956@users.noreply.github.com>
2023-03-07 16:08:39 +0900

    feat: 매칭 참가자만 평가 폼 접근 가능

--- matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/common/domain/CreatePageable.java
+++ matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/common/domain/CreatePageable.java
@@ -7,9 +7,9 @@ import org.springframework.data.domain.Pageable;
 public class CreatePageable {
 
     public static Pageable createPageable(int page) {
-//        Sort.Direction direction = Sort.Direction.DESC;
-//        Sort sort = Sort.by(direction, "modifiedAt");
-        Pageable pageable = PageRequest.of(page - 1, 5);
+//        Sort.Direction direction = Sort.Direction.ASC;
+//        Sort sort = Sort.by(direction, "id");
+        Pageable pageable = PageRequest.of(page - 1, 50);
         return pageable;
     }
 }
