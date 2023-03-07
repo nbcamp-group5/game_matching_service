a7c2a7ae9bdd9a367dfd89d6855afab1b443bcb3
jeonjeahyeong <117060390+jaehyeongj@users.noreply.github.com>
2023-03-07 10:49:24 +0900

    Update stop.sh

--- matchingService/scripts/stop.sh
+++ matchingService/scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/springboot_shop-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/matchingService/build/libs/matchingService.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
