8a3fe27404c6a50ad7d96b33f2d0da7c5671fe9d
jeonjeahyeong <117060390+jaehyeongj@users.noreply.github.com>
2023-03-09 16:55:17 +0900

    Update stop.sh

--- matchingService/scripts/stop.sh
+++ matchingService/scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/matchingService/build/libs/matchingService.jar"
+JAR_FILE="$PROJECT_ROOT/matchingService.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
